6c79636e6dddbe0658dc6e9678fd4736669a3218
abaezacastro <andres.baeza@gmail.com>
2017-04-28 15:31:10 -0500

    less parameter values to test

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ eficiencia_nuevainfra = np.linspace(0.005, 0.01, 2)
 
 eficiencia_mantenimiento = np.linspace( 0.005, 0.01, 2)
 
-Lambda = np.linspace( 0, 1.0 / (100.0 * 365.0), 3)
+Lambda = np.linspace( 0, 1.0 / (100.0 * 365.0), 2)
 
-factor_subsidencia = np.linspace( 0.001, 0.05, 3)
+factor_subsidencia = np.linspace( 0.001, 0.05, 2)
 
-recursos_para_distribucion = np.linspace( 500, 2000, 3)
+recursos_para_distribucion = np.linspace( 500, 2000, 2)
 
-recursos_para_mantenimiento = np.linspace( 500, 2000, 3)
+recursos_para_mantenimiento = np.linspace( 500, 2000, 2)
 
-recursos_nuevainfrastructura = np.linspace( 500, 2000, 3)
+recursos_nuevainfrastructura = np.linspace( 500, 2000, 2)
 
-requerimiento_deagua = np.linspace( 0.007, 0.1, 3)
+requerimiento_deagua = np.linspace( 0.007, 0.1, 2)
 
 
