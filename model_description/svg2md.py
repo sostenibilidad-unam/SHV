08b4c4e7bf6a34fbdafcd92997c76cfb26b49e4e
Fidel Serrano <serranoycandela@gmail.com>
2017-11-03 11:45:08 -0600

    onclick

--- model_description/svg2md.py
+++ model_description/svg2md.py
@@ -3,30 +3,15 @@ from slugify import slugify
 
 tree = etree.parse("diagrams2.svg")
 root = tree.getroot()
-
+root.set('id', "svgroot")
+root.set('width', "60%")
+root.set('height', "60%")
 
 slugs = list()
 
-for e in root.findall('{http://www.w3.org/2000/svg}text'):
-    text = []
-    for tspan in e.getchildren():
-        if tspan.text is not None:
-            text.append(tspan.text.strip())
-    if text:
-        text = u" ".join(text)
-        slug = slugify(text)
-        n = slugs.count(slug)
-        slugs.append(slug)
-        if n:
-            slug += unicode(n)
-
-        liga = etree.Element('ns0:a', {'xlink:href': slug + '.md'})
-        liga.append(e)
-        root.append(liga)
-        root.remove(e)
-
-
 for ch in root.getchildren():
+    text = []
+    slug = slugify(u"x")
     for e in ch.findall('{http://www.w3.org/2000/svg}text'):
         text = []
         for tspan in e.getchildren():
@@ -39,13 +24,44 @@ for ch in root.getchildren():
             slugs.append(slug)
             if n:
                 slug += unicode(n)
+            with open(slug + '.md', "w") as pluma:
+                pluma.write(text)
+            # liga = etree.Element('ns0:a', {'href': slug + '.md'})
+            # liga.append(e)
+            # ch.append(liga)
+            # ch.remove(e)
+    if text:
+        # ch.set('onclick', "load_info('" + text + "');")        
+        ch.set('id', slug)
+        ch.set('onclick', "window.parent.svgElementClicked('" + slug + "');")  
+        
+        
+for e in root.findall('{http://www.w3.org/2000/svg}text'):
+    text = []
+    slug = slugify(u"x")
+    for tspan in e.getchildren():
+        if tspan.text is not None:
+            text.append(tspan.text.strip())
+    if text:
+        text = u" ".join(text)
+        slug = slugify(text)
+        n = slugs.count(slug)
+        slugs.append(slug)
+        if n:
+            slug += unicode(n)
+        with open(slug + '.md', "w") as pluma:
+            pluma.write(text)
+        # liga = etree.Element('ns0:a', {'href': slug + '.md'})
+        # liga.append(e)
+        # root.append(liga)
+        # root.remove(e)
+        e.set('id', slug)
+        e.set('onclick', "window.parent.svgElementClicked('" + slug + "');")
 
-            liga = etree.Element('ns0:a', {'xlink:href': slug + '.md'})
-            liga.append(e)
-            ch.append(liga)
-            ch.remove(e)
+
+
 
 from pprint import pprint
-pprint( slugs)
+pprint(slugs)
 
 tree.write('aguas.svg')
