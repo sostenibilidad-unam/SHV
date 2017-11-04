efd369c2196dc42bce218640e5b7bd8260a18635
Fidel Serrano <serranoycandela@gmail.com>
2017-11-04 12:30:49 -0600

    add onmouseover

--- model_description/svg2md.py
+++ model_description/svg2md.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as etree
 from slugify import slugify
 
-tree = etree.parse("diagrams2.svg")
+tree = etree.parse("diagrams5.svg")
 root = tree.getroot()
 root.set('id', "svgroot")
 root.set('width', "60%")
@@ -18,6 +18,7 @@ for ch in root.getchildren():
             if tspan.text is not None:
                 text.append(tspan.text.strip())
         if text:
+            
             text = u" ".join(text)
             slug = slugify(text)
             n = slugs.count(slug)
@@ -26,6 +27,7 @@ for ch in root.getchildren():
                 slug += unicode(n)
             with open(slug + '.md', "w") as pluma:
                 pluma.write(text)
+            e.set('id', slug + "-txt")
             # liga = etree.Element('ns0:a', {'href': slug + '.md'})
             # liga.append(e)
             # ch.append(liga)
@@ -34,6 +36,8 @@ for ch in root.getchildren():
         # ch.set('onclick', "load_info('" + text + "');")        
         ch.set('id', slug)
         ch.set('onclick', "window.parent.svgElementClicked('" + slug + "');")  
+        ch.set('onmouseover', "document.getElementById('" + slug + "-txt').setAttribute('style', 'fill: #000000;text-anchor:middle;font-family:sans-serif;font-style:normal;font-weight:bold');")
+        ch.set('onmouseout', "document.getElementById('" + slug + "-txt').setAttribute('style', 'fill: #000000;text-anchor:middle;font-family:sans-serif;font-style:normal;font-weight:normal');")
         
         
 for e in root.findall('{http://www.w3.org/2000/svg}text'):
@@ -51,13 +55,14 @@ for e in root.findall('{http://www.w3.org/2000/svg}text'):
             slug += unicode(n)
         with open(slug + '.md', "w") as pluma:
             pluma.write(text)
+        e.set('id', slug + "-txt")
         # liga = etree.Element('ns0:a', {'href': slug + '.md'})
         # liga.append(e)
         # root.append(liga)
         # root.remove(e)
-        e.set('id', slug)
         e.set('onclick', "window.parent.svgElementClicked('" + slug + "');")
-
+        e.set('onmouseover', "document.getElementById('" + slug + "-txt').setAttribute('style', 'fill: #000000;text-anchor:left;font-family:sans-serif;font-style:normal;font-weight:bold');")
+        e.set('onmouseout', "document.getElementById('" + slug + "-txt').setAttribute('style', 'fill: #000000;text-anchor:left;font-family:sans-serif;font-style:normal;font-weight:normal');")
 
 
 
