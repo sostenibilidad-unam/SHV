ffc096049aea8afcd0e76d0bfeb1d9f546294944
serranoycandela <serranoycandela@gmail.com>
2017-11-14 22:27:47 -0600

    code snipets

---
+++ extract_snippets_from_netlogo.py
@@ -0,0 +1,38 @@
+import json
+import io
+slugs = []
+with open('./model_description/slugs.json') as json_data:
+    slugs = json.load(json_data)
+    
+for slug in slugs:
+    print(slug)
+    with io.open("./model_description/"+slug+".html", mode="w", encoding="utf-8") as pluma:
+        pluma.write(u"<style>body {background-color: lightblue;}</style><body>")
+        pluma.write(u"<h1><center><b>"+slug+u"</b></center></h1><br/ >")
+        with io.open("ABM-Empirical-MexicoCity_6.html", mode="r", encoding="utf-8") as f:
+            escribo = False
+            for line in f:
+               if ";"+slug+";" in line:
+                   escribo = True
+                   pluma.write(u"<pre><b>file: ABM-Empirical-MexicoCity_6.nlogo</b><br/ >")
+                   pluma.write(u"     .<br/ >     .<br/ >     .<br/ ><br/ >")
+               if ";/"+slug+";" in line:
+                   escribo = False
+                   pluma.write(u"<br/ >     .<br/ >     .<br/ >     .<br/ >")
+                   pluma.write(u"</pre>")
+               if escribo:
+                   pluma.write(line)
+        with io.open("value_funtions.html", mode="r", encoding="utf-8") as f:
+            escribo = False
+            for line in f:
+               if ";"+slug+";" in line:
+                   escribo = True
+                   pluma.write(u"<pre><b>file: value_funtions.nls</b><br/ >")
+                   pluma.write(u"     .<br/ >     .<br/ >     .<br/ ><br/ >")
+               if ";/"+slug+";" in line:
+                   escribo = False
+                   pluma.write(u"<br/ >     .<br/ >     .<br/ >     .<br/ >")
+                   pluma.write(u"</pre>")
+               if escribo:
+                   pluma.write(line)
+        pluma.write(u"</body>")
--- model_description/svg2md.py
+++ model_description/svg2md.py
@@ -8,7 +8,7 @@ root.set('width', "60%")
 root.set('height', "60%")
 
 slugs = list()
-
+slugs1 = list()
 for ch in root.getchildren():
     text = []
     slug = slugify(u"x")
@@ -23,8 +23,10 @@ for ch in root.getchildren():
             slug = slugify(text)
             n = slugs.count(slug)
             slugs.append(slug)
+            
             if n:
                 slug += unicode(n)
+            slugs1.append(slug)
             with open(slug + '.md', "w") as pluma:
                 pluma.write(text)
             e.set('id', slug + "-txt")
@@ -36,8 +38,8 @@ for ch in root.getchildren():
         # ch.set('onclick', "load_info('" + text + "');")        
         ch.set('id', slug)
         ch.set('onclick', "window.parent.svgElementClicked('" + slug + "');")  
-        ch.set('onmouseover', "document.getElementById('" + slug + "-txt').setAttribute('style', 'fill: #000000;text-anchor:middle;font-family:sans-serif;font-style:normal;font-weight:bold');")
-        ch.set('onmouseout', "document.getElementById('" + slug + "-txt').setAttribute('style', 'fill: #000000;text-anchor:middle;font-family:sans-serif;font-style:normal;font-weight:normal');")
+        ch.set('onmouseover', "document.getElementById('" + slug + "-txt').style.fontWeight = 'bold';")
+        ch.set('onmouseout', "if (window.parent.current_selection != '" + slug + "') document.getElementById('" + slug + "-txt').style.fontWeight = 'normal';")
         
         
 for e in root.findall('{http://www.w3.org/2000/svg}text'):
@@ -53,6 +55,7 @@ for e in root.findall('{http://www.w3.org/2000/svg}text'):
         slugs.append(slug)
         if n:
             slug += unicode(n)
+        slugs1.append(slug)
         with open(slug + '.md', "w") as pluma:
             pluma.write(text)
         e.set('id', slug + "-txt")
@@ -61,12 +64,14 @@ for e in root.findall('{http://www.w3.org/2000/svg}text'):
         # root.append(liga)
         # root.remove(e)
         e.set('onclick', "window.parent.svgElementClicked('" + slug + "');")
-        e.set('onmouseover', "document.getElementById('" + slug + "-txt').setAttribute('style', 'fill: #000000;text-anchor:left;font-family:sans-serif;font-style:normal;font-weight:bold');")
-        e.set('onmouseout', "document.getElementById('" + slug + "-txt').setAttribute('style', 'fill: #000000;text-anchor:left;font-family:sans-serif;font-style:normal;font-weight:normal');")
+        e.set('onmouseover', "document.getElementById('" + slug + "-txt').style.fontWeight = 'bold';")
+        e.set('onmouseout', "if (window.parent.current_selection != '" + slug + "') document.getElementById('" + slug + "-txt').style.fontWeight = 'normal';")
 
 
-
+import json
+with open('slugs.json', 'w') as outfile:
+    json.dump(slugs1, outfile)
 from pprint import pprint
 pprint(slugs)
 
-tree.write('aguas.svg')
+tree.write('aguas2.svg')
