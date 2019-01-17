c46656c955e318560e0b2d5b0cc7367fdb272e63
Andres Baeza <andres.baeza@gmail.com>
2019-01-16 19:18:42 -0700

    This function reads an unweighted matrix and change values in selected blocks

--- ABM_Rversion/MEGADAPT_APP/initial_parameter_values.R
+++ ABM_Rversion/MEGADAPT_APP/initial_parameter_values.R
@@ -4,7 +4,7 @@ effectivity_newInfra=0.07
 effectivity_mantenimiento=0.07
 time_simulation=5
 decay_infra=0.01
-Budget=1000
+Budget=1800
 path_to_source<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/ABM_Rversion/MEGADAPT_APP/" #change path to use it
 path_to_output<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/outputs/" #change path to use it
 
