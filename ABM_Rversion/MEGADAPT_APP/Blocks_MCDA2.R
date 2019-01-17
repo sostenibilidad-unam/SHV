6bdd437954abf799513e1896d470aab28ebe1a43
Andres Baeza <andres.baeza@gmail.com>
2019-01-17 13:23:38 -0700

    Calculate limit matrix from weighted supermatrix

--- ABM_Rversion/MEGADAPT_APP/Blocks_MCDA2.R
+++ ABM_Rversion/MEGADAPT_APP/Blocks_MCDA2.R
@@ -6,6 +6,7 @@
 
 
 modify_block_row=function(path_td, unwaited_supermatrix, block, col_to_modify, values){
+  if(sum(values)!=1)stop("values must sum to 1")
   UWM=read.csv(paste(path_td),sep=",",skip = 1,header = T)
   y_cors=which(UWM$X!="")
   x_cors=2+y_cors
@@ -21,8 +22,10 @@ modify_block_row=function(path_td, unwaited_supermatrix, block, col_to_modify, v
   }
   a <- category_coors[[paste(block[1])]][1] 
   b <- category_coors[[paste(block[1])]][2] 
-  c <- category_coors[[paste(block[2])]][1] + col_to_modify - 1
-  unwaited_supermatrix[a:b, c] <- values
+  cc <- category_coors[[paste(block[2])]][1] + col_to_modify - 1
+  if(length(values)!=length(a:b))stop("number of rows to change must be the same length as the length of the col_to_modify column in block")
+
+  unwaited_supermatrix[a:b, cc] <- values
   return(unwaited_supermatrix)
 }
 
@@ -34,10 +37,14 @@ get_matrix <- function(path_td){
 }
 
 
-x = get_matrix("../../data/I072816_OTR.weighted.csv")
-xm = modify_block_row("../../data/I072816_OTR.weighted.csv", x, c(1,1),1,c(0.2,0.2,0.2,0.2,0.2))
+x = get_matrix("../../data/supermatrices/SACMEX_unweighted_SESMO.csv")
+xm = modify_block_row(path_td ="../../data/supermatrices/SACMEX_unweighted_SESMO.csv", x, block =c(1,1), col_to_modify =1,values =  c(0.2,0.8))
 xm = modify_block_row("../../data/I072816_OTR.weighted.csv", xm, c(1,1),2,c(0.2,0.2,0.2,0.2,0.2))
 xm = modify_block_row("../../data/I072816_OTR.weighted.csv", xm, c(1,1),3,c(0.2,0.2,0.2,0.2,0.2))
 xm = modify_block_row("../../data/I072816_OTR.weighted.csv", xm, c(1,2),1,c(0.1,0.1,0.2,0.3,0.3))
 xm = modify_block_row("../../data/I072816_OTR.weighted.csv", xm, c(1,2),2,c(0.1,0.1,0.2,0.3,0.3))
 xm = modify_block_row("../../data/I072816_OTR.weighted.csv", xm, c(1,2),3,c(0.1,0.1,0.2,0.3,0.3))  
+###############################################################
+#new weighted super matrix
+weighted_supermatrix=sweep(xm,MARGIN = 2,colSums(xm),FUN = "/")
+###################################################################
