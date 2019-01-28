c30e39fedd592f7c9e2ab77b934310bff202de73
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-01-28 13:04:02 -0600

    include scenario argument, relative path for output

--- ABM_Rversion/MEGADAPT_APP/run_MEGADAPT_Cluster.R
+++ ABM_Rversion/MEGADAPT_APP/run_MEGADAPT_Cluster.R
@@ -6,16 +6,16 @@ effectivity_mantenimiento=as.numeric(args[2])
 decay_infra=as.numeric(args[3])
 time_simulation=as.numeric(args[4])
 Budget=as.numeric(args[5])
+scenario=as.numeric(args[6])
 
 #Generate and id for the simulation based on the value
 #of the argument
-sim_id_output=sprintf("effectivity_newInfra=%s-effectivity_mantenimiento=%s-decay_infra=s%-Budget=%s",effectivity_newInfra,effectivity_mantenimiento,decay_infra,Budget)
-path_to_output<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/outputs/" #change path to use it in patung
+sim_id_output=sprintf("effectivity_newInfra=%s-effectivity_mantenimiento=%s-decay_infra=%s-Budget=%s-scenario=%s",effectivity_newInfra,effectivity_mantenimiento,decay_infra,Budget,scenario)
+path_to_output<-"../../outputs/" #change path to use it in patung
 
 #climate scenario
-#path_to_source<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/ABM_Rversion/MEGADAPT_APP/"
 path_to_source<-"."
 setwd(path_to_source)
-#run setup.R and cycle.R 
-    source("setup.R")
-    source("cycle.R")
+#run setup.R and cycle.R
+source("setup.R")
+source("cycle.R")
