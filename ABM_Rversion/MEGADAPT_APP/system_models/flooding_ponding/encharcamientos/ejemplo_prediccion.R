d65b5c66f8c9e7486c811a7c1e1332f6f1ca270f
serranoycandela <serranoycandela@gmail.com>
2019-02-13 12:25:03 -0600

    ejemplo encharcamientos

--- ABM_Rversion/MEGADAPT_APP/system_models/flooding_ponding/encharcamientos/ejemplo_prediccion.R
+++ ABM_Rversion/MEGADAPT_APP/system_models/flooding_ponding/encharcamientos/ejemplo_prediccion.R
@@ -8,32 +8,32 @@ load(file = "mod_en_reg5.rda")
 load(file = "mod_en_reg6.rda")
 load(file = "mod_en_reg7.rda")
 
-f_prec_v <- c(seq(100000, 1000000, length.out = 10))
+f_prec_v <- c(seq(353947, 1000000, length.out = 10))
 f_esc <- c(seq(0, 0, length.out = 10))
-n_tramos <- c(seq(40, 50, length.out = 10))
-q100 <- c(seq(2, 100, length.out = 10))
+n_tramos <- c(seq(194, 240, length.out = 10))
+q100 <- c(seq(144, 244, length.out = 10))
 bombeo_tot <- c(seq(0, 0, length.out = 10))
-rejillas <- c(seq(0, 20, length.out = 10))
+rejillas <- c(seq(88, 100, length.out = 10))
 
 dataframe <- data.frame(f_prec_v,f_esc,n_tramos,q100,bombeo_tot,rejillas)
 
 ## Se crean datos de prueba
-datos_prueba <- structure(list(f_prec_v = 1000000,
+datos_prueba <- structure(list(f_prec_v = 353947,
                                f_esc = 0,
-                               n_tramos = 40,
-                               q100 = 100,
+                               n_tramos = 194,
+                               q100 = 144,
                                bombeo_tot = 0,
-                               rejillas = 1),
+                               rejillas = 88),
                           row.names = c(NA, -1L), class = c("data.frame"))
 head(datos_prueba)
 
 ## Se hace la predicción
-kk <- predict(modelo_en_region_6, # Modelo de region 1
+kk <- predict(modelo_en_region_5, # Modelo de region 1
         datos_prueba, # Una fila con observaciones de todas las variables
         n.trees = 9566, # Número de árboles que usa el modelo
         type = "response")
 
-kkk <- predict(modelo_en_region_6, # Modelo de region 1
-               dataframe, # Una fila con observaciones de todas las variables
+kkk <- predict(modelo_en_region_5, # Modelo de region 1
+               dataframe, # Una dataframe con observaciones de todas las variables
                n.trees = 9566, # Número de árboles que usa el modelo
                type = "response")
