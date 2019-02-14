34b66727d74757c23d6985b2b2c678264588266f
Andres Baeza <andres.baeza@gmail.com>
2019-02-14 10:40:50 -0700

    update new variable to include flooding as outcome

--- ABM_Rversion/MEGADAPT_APP/utils/initial_parameter_values.R
+++ ABM_Rversion/MEGADAPT_APP/utils/initial_parameter_values.R
@@ -6,12 +6,12 @@ time_simulation=5
 decay_infra=0.01
 Budget=1800
 climate_scenario=1
-path_to_source<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/ABM_Rversion/MEGADAPT_APP/" #change path to use it
+#path_to_source<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/ABM_Rversion/MEGADAPT_APP/" #change path to use it
 
-path_to_output<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/outputs/" #change path to use it in patung
+#path_to_output<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/outputs/" #change path to use it in patung
 
 #Generate and id for the simulation based on the value
 #of the argument
 sim_id_output=sprintf("effectivity_newInfra=%s-effectivity_mantenimiento=%s-decay_infra=%s-Budget=%s-climate_scenario=%s",effectivity_newInfra,effectivity_mantenimiento,decay_infra,Budget,climate_scenario)
 #set the directory path
-setwd(path_to_source)
+#setwd(path_to_source)
